84c9f6cf7289b385ee7e065b4c2fac662b9b69e4
Brian Moriguchi <brian@beckos.com>
2022-07-06 15:14:15 -0700

    Initial Commit

---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Tatsuya Moriguchi on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
