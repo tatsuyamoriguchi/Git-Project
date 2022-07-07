b0f5e332fbff8578684ade508ea29f8e3c646cbe
Brian Moriguchi <brian@beckos.com>
2022-07-06 17:33:30 -0700

    changed the link to blue

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             NavigationLink(destination: ModalView()) {
                     Text("Go Next")
         
-            }.foregroundColor(.green)
+            }.foregroundColor(.blue)
             .navigationTitle("Xcode and Git")
                 
             }
