4a2e91f4761070f39cfcddbb9dcecb5a009ffbed
Brian Moriguchi <brian@beckos.com>
2022-07-06 17:13:09 -0700

    Added a VStack

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            VStack {
+                
+            Text("This is the Content View.")
             NavigationLink(destination: ModalView()) {
                     Text("Go Next")
         
             }.foregroundColor(.green)
             .navigationTitle("Xcode and Git")
+                
+            }
         }
     }
 }
